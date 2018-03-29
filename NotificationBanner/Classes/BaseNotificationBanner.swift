976c49b25ca56cb946be57977f2ad3ba6fb77591
Andrii Tkachuk <atkac@softserveinc.com>
2018-03-29 18:29:24 +0300

    Remove unnecessary changes

--- NotificationBanner/Classes/BaseNotificationBanner.swift
+++ NotificationBanner/Classes/BaseNotificationBanner.swift
@@ -321,8 +321,6 @@ public class BaseNotificationBanner: UIView {
             NotificationCenter.default.post(name: NotificationBanner.BannerWillAppear, object: self, userInfo: notificationUserInfo)
             delegate?.notificationBannerWillAppear(self)
             
-            self.isDisplaying = true
-            
             UIView.animate(withDuration: 0.5,
                            delay: 0.0,
                            usingSpringWithDamping: 0.7,
@@ -336,6 +334,7 @@ public class BaseNotificationBanner: UIView {
                 NotificationCenter.default.post(name: NotificationBanner.BannerDidAppear, object: self, userInfo: self.notificationUserInfo)
                 self.delegate?.notificationBannerDidAppear(self)
                 
+                self.isDisplaying = true
                 let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.onTapGestureRecognizer))
                 self.addGestureRecognizer(tapGestureRecognizer)
                 
