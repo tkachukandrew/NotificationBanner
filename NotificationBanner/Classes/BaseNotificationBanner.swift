012c4f2df8728a817d573f69f34c889eb58aa683
Andrii Tkachuk <atkac@softserveinc.com>
2018-03-30 11:33:23 +0300

    Add changes from commit 65e517e

--- NotificationBanner/Classes/BaseNotificationBanner.swift
+++ NotificationBanner/Classes/BaseNotificationBanner.swift
@@ -321,6 +321,8 @@ public class BaseNotificationBanner: UIView {
             NotificationCenter.default.post(name: NotificationBanner.BannerWillAppear, object: self, userInfo: notificationUserInfo)
             delegate?.notificationBannerWillAppear(self)
             
+            self.isDisplaying = true
+            
             UIView.animate(withDuration: 0.5,
                            delay: 0.0,
                            usingSpringWithDamping: 0.7,
@@ -334,7 +336,6 @@ public class BaseNotificationBanner: UIView {
                 NotificationCenter.default.post(name: NotificationBanner.BannerDidAppear, object: self, userInfo: self.notificationUserInfo)
                 self.delegate?.notificationBannerDidAppear(self)
                 
-                self.isDisplaying = true
                 let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.onTapGestureRecognizer))
                 self.addGestureRecognizer(tapGestureRecognizer)
                 
